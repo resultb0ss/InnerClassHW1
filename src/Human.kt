e1eaf374b1b7fff95d1feda1a52d8f7ebdcafcce
Admin <reteamboss@gmail.com>
2024-08-31 15:22:12 +0300

    Задание 1

--- src/Human.kt
+++ src/Human.kt
@@ -1,2 +1,29 @@
-class Human {
+class Human(val name: String) {
+
+   open inner class Head(){
+       override fun toString(): String {
+           return "Я голова умею думать"
+       }
+
+       open fun say(){
+           println("Голова умеет не только думать но и говорить")
+       }
+   }
+
+    open inner class Legs(){
+
+        open fun info(): String {
+            return "Ноги ходят"
+        }
+
+        open fun go(){
+            println("Ноги дают возможность ходить")
+        }
+
+
+    }
+
+    override fun toString(): String {
+        return "$name имею руки и ноги"
+    }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,13 @@
 fun main() {
-    println("Hello World!")
+    var humanJoe = Human("Joe")
+
+    println(humanJoe.toString())
+
+    println("------------------")
+    humanJoe.Head().say()
+    println(humanJoe.Head().toString())
+
+    println("----------------")
+    humanJoe.Legs().go()
+    println(humanJoe.Legs().info())
 }
